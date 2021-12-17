d2cb8a2c7d105a0ad9fe99c44b677fbb8c2cbcb7
Nicolas Garcia Rosell <nicolasgarciarosell@nicolass-macbook-air.local>
2021-12-17 15:09:59 -0500

    rewrote functions, add rangeOfBills() and billCalc2()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,13 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.DateFormatSymbols;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 // ADD TRY CATCH WHERE NECESSARY
@@ -21,147 +24,88 @@ public class Main {
         // *GOAL*
         // read file of accounts with their name, payment day and amount due
         // terminates with "0"
+        // modify in.txt file in order to see your bills!
         // EXAMPLE IN.TXT
-        //        crunchyroll 1 9.02
-        //        chegg 28 14.95
-        //        car 20 275.74
-        //        spotify 19 5.26
-        //        phone 19 84.04
-        //        0
-
 
 
         // could be a global private static but would need a try-catch outside of main!
-        // scanner for file
+        // scanner for file is "sc"
         Scanner sc = new Scanner(infile);
 
-        //Make an arrayList of the accounts
         List<Account> accounts = new ArrayList<>();
 
         accounts = readFile(sc, accounts);
 
-        billCalculator(accounts);
+        List<LocalDate> listOfDates = rangeOfBills();
 
+        billCalc2(listOfDates, accounts);
+
+        scan.close();
         sc.close();
 
-
     }
 
-    public static void billCalculator(List<Account> accounts){
+    public static void billCalc2(List<LocalDate> dates, List<Account> accounts){
 
-        // ask for dates and separate the month and days from both inputs
-        // *possible move to a method outside of main
-        System.out.println("Choose a start date (mmdd)");
-        int start, startDay, startMonth;
+        double totalBill = 0.0;
 
 
-        while(true) {
-            start = scan.nextInt();
-            if(start < 1000){
-                System.out.println("Error: Invalid Entry");
-                System.out.println("Must be (mmdd) format");
-                System.out.println("Rerun App!");
+        for (Account account: accounts){
+            double accountBill = 0.0;
 
+            for(LocalDate date: dates){
+                if(account.getDate() == date.getDayOfMonth()){
+                    accountBill += account.getCost();
+                    totalBill += account.getCost();
+                }
             }
-            startDay = start % 100;
-            startMonth = (start - startDay)/100;
-
-            if ((start % 100) > 31 || startMonth > 12) {
-                System.out.println("Invalid date: try again");
-            }
-            else
-                break;
-        }
-
-        System.out.println("Choose an end date (mmdd)");
-        int end, endDay, endMonth;
-
-        while(true) {
-            end = scan.nextInt();
-            if(end < 1000){
-                System.out.println("Error: Invalid Entry");
-                System.out.println("Must be (mmdd) format");
-                System.out.println("Rerun App!");
-            }
-            endDay = end % 100;
-            endMonth = (end - endDay)/100;
-
-            if ((end % 100) > 31 || endMonth > 12) {
-                System.out.println("Invalid date: try again");
-            }
-            else
-                break;
-        }
-
-        // choose algorithm to calculate total money spent on bills between
-        // the two dates.
-
-        double totalCost = checkBill(startMonth, startDay, endMonth, endDay, accounts);
-
-        // Two methods to get names for Month
-        String nameStart = monthName(startMonth);
-        String nameEnd = monthName(endMonth);
-
-        // use printf to format decimals
-        System.out.printf("From "+ nameStart +" "+startDay+" to "+nameEnd+" "+endDay
-                +" , you will pay $%.2f", totalCost);
-
-    }
-
-    public static double checkBill(int sM, int sD, int eM, int eD, List<Account> accounts){
-
-        int multiplier;
-        double sumOfAllAccounts = 0;
-        double total = 0;
-
-        // only calculates bill within a 1-year period!!
-
-        if((eM < sM) || (eM == sM && eD <= sD)){
-            eM = eM + 12;
+            System.out.printf("Account Name: "+account.getName()+"\t\t Total paid over period: $%.2f\n", accountBill);
         }
 
 
-        multiplier = eM - sM - 1;
+        // format date
+        LocalDate start = dates.get(0);
+        LocalDate end = dates.get(dates.size()-1);
+        String formattedStartDate = start.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        String formattedEndDate = end.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
 
-        System.out.println("You will pay this much for each of your accounts: ");
-        for(Account x: accounts)
-        {
-            double tempTotal = 0;
-            double currentAccountTotal;
-            if(x.getDate() >= sD) {
-                total = total + x.getCost();
-                tempTotal = tempTotal + x.getCost();
+
+        System.out.printf("\nFrom "+ formattedStartDate+" to "+ formattedEndDate+" , you will pay $%.2f toward your bills!", totalBill);
+
+
+    }
+
+    public static List<LocalDate> rangeOfBills() {
+
+        boolean notValid = true;
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+        while(notValid) {
+            try {
+                System.out.println("Start Date (mm/dd/yyyy)");
+                String day1 = scan.nextLine();
+                System.out.println("End Date (mm/dd/yyyy)");
+                String day2 = scan.nextLine();
+
+                // year/month/dayOfMonth
+                startDate = LocalDate.of(Integer.parseInt(day1.substring(6,10)),Integer.parseInt(day1.substring(0,2)),Integer.parseInt(day1.substring(3,5)));
+                endDate = LocalDate.of(Integer.parseInt(day2.substring(6,10)),Integer.parseInt(day2.substring(0,2)),Integer.parseInt(day2.substring(3,5))+1);
+
+                notValid = false;
+            } catch (Exception e) {
+                System.out.println("Date is invalid, check Exception, must be in format (mm/dd/yyyy)");
+                System.out.println("Ex. 07/02/2020");
+                System.out.println("Error: "+ e);
+
             }
-
-            if(x.getDate() <= eD) {
-                total = total + x.getCost();
-                tempTotal = tempTotal + x.getCost();
-
-            }
-
-            currentAccountTotal =  tempTotal + (x.getCost() * multiplier);
-
-            // printf to format decimals (%.2f)
-            System.out.printf("$%.2f for "+ x.getName()+"\n", currentAccountTotal);
-
-            sumOfAllAccounts = sumOfAllAccounts + x.getCost();
-
         }
 
-        total = total + (sumOfAllAccounts * multiplier);
-
-        return total;
-
-    }
-
-    public static String monthName(int month){
-
-        // return month in string format
-         return new DateFormatSymbols().getMonths()[month-1];
+        // returns List<LocalDate> of dates within the range
+        return startDate.datesUntil(endDate).collect(Collectors.toList());
 
     }
 
-    public static List readFile(Scanner sc, List<Account> accounts){
+    public static List<Account> readFile(Scanner sc, List<Account> accounts){
 
         Account account;
 
@@ -169,17 +113,22 @@ public class Main {
 
         while(!n.equals("0"))
         {
+            // nameBill/dateBill/priceBill
             account = new Account(n, sc.nextInt(), sc.nextDouble());
+            if(account.getDate() >= 31){
+                System.out.println("You're bill date will be changed to the 28th for "+account.getName()+"!");
+                account.setDate(28);
+            }
             accounts.add(account);
             // keep reading first string of line until "0"
             n = sc.next();
 
         }
 
-        System.out.println("You have the following accounts: \n");
+        System.out.println("\nYou have the following accounts: ");
         for(Account x : accounts)
         {
-            System.out.println("Name: "+x.getName()+" Cost: "+x.getCost()+" Date:" + x.getDate());
+            System.out.println("Name: "+x.getName()+"\t\t Cost: $"+x.getCost()+"\t\t Date: " + x.getDate());
         }
         System.out.println("=======================================");
 
@@ -187,6 +136,5 @@ public class Main {
         return accounts;
 
     }
-
 
 }
