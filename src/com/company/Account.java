a989c8d5d6cf1a80a1510b215659c50b2161cd67
Nicolas Garcia Rosell <garciaroselln@hotmail.com>
2022-02-06 15:05:18 -0500

    add different bill types and their applications

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,21 +1,97 @@
 package com.company;
 
 public class Account {
+    private String type;
     private String name;
     private int date;
     private double cost;
+    private double balance = -1;
+    private double newCost;
     private int month = 0;
 
-    public Account(String name, int date, double cost) {
+    //billType/billName/billDate/billAmount/balanceAmount/newBillAmount
+
+    public Account(String type, String name, int date, double cost) {
+        this.type = type;
         this.name = name;
         this.date = date;
         this.cost = cost;
     }
-    public Account(String name, int date, double cost, int month) {
+    public Account(String type, String name, int date, double cost, int month) {
+        this.type= type;
         this.name = name;
         this.date = date;
         this.cost = cost;
         this.month = month;
+    }
+
+    public Account(String type, String name, int date, double cost, double balance) {
+        this.type = type;
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.balance = balance;
+    }
+    public Account(String type, String name, int date, double cost, double balance, int month) {
+        this.type = type;
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.balance = balance;
+        this.month = month;
+    }
+
+
+    public Account(String type, String name, int date, double cost, double balance, double newCost) {
+        this.type = type;
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.balance = balance;
+        this.newCost = newCost;
+    }
+    public Account(String type, String name, int date, double cost, double balance, double newCost, int month) {
+        this.type = type;
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.balance = balance;
+        this.newCost = newCost;
+        this.month = month;
+    }
+
+    public Account(){
+        name = "";
+        date = 0;
+        cost = 0;
+        type = "";
+        balance = 0;
+        newCost = 0;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getNewCost() {
+        return newCost;
+    }
+
+    public void setNewCost(double newCost) {
+        this.newCost = newCost;
     }
 
     public String getName() {
@@ -48,12 +124,6 @@ public class Account {
 
     public void setMonth(int month) {
         this.month= month;
-    }
-
-    public Account(){
-        name = "";
-        date = 0;
-        cost = 0;
     }
 
     @Override
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -45,24 +45,78 @@ public class Main {
 
     public static void billCalc(List<LocalDate> dates, List<Account> accounts){
 
+        // DETERMINE NEW BALANCE AND IF BALANCE BECOMES 0, DELETE ACCOUNT(BALANCE) OR
+        // DETERMINE NEW BILL FOR ACCOUNT ONCE BALANCE HITS 0 (BALANCE AND BILL)
+
+
         double totalBill = 0.0;
 
         for (Account account: accounts){
             double accountBill = 0.0;
 
-            for(LocalDate date: dates){
 
-                if((account.getDate() == date.getDayOfMonth()) && (account.getMonth() == date.getMonthValue())) {
-                    accountBill += account.getCost();
-                    totalBill += account.getCost();
-                }else if(account.getDate() == date.getDayOfMonth() && account.getMonth() == 0){
-                    accountBill += account.getCost();
-                    totalBill += account.getCost();
+            for(LocalDate date: dates){
+                if(account.getBalance()>0 || account.getBalance()==-1){
+                    // if accountBill has "special date" ie "1/26"
+                    if((account.getDate() == date.getDayOfMonth()) && (account.getMonth() == date.getMonthValue())) {
+
+                        accountBill += account.getCost();
+                        totalBill += account.getCost();
+
+                        if(account.getType().equalsIgnoreCase("Balance and Bill")){
+                            if(account.getBalance()>0){
+                                account.setBalance(account.getBalance()-(account.getCost()-account.getNewCost()));
+                                if(account.getBalance()==0){
+                                    account.setCost(account.getNewCost());
+                                    account.setBalance(-1);
+                                }
+                            }
+                        }else if(account.getType().equalsIgnoreCase("Balance")){
+                            if(account.getBalance()>0){
+                                account.setBalance(account.getBalance()-account.getCost());
+                            }
+                        }
+
+                    }
+                    // if accountBill has normal date ie "15"
+                    else if(account.getDate() == date.getDayOfMonth() && account.getMonth() == 0){
+                        accountBill += account.getCost();
+                        totalBill += account.getCost();
+
+                        if(account.getType().equalsIgnoreCase("Balance and Bill")){
+                            if(account.getBalance()>0){
+                                account.setBalance(account.getBalance()-(account.getCost()-account.getNewCost()));
+                                if(account.getBalance()==0){
+                                    account.setCost(account.getNewCost());
+                                    account.setBalance(-1);
+                                }
+                            }
+                        }else if(account.getType().equalsIgnoreCase("Balance")){
+                            if(account.getBalance()>0){
+                                account.setBalance(account.getBalance()-account.getCost());
+                            }
+                        }
+
+                    }
+
                 }
+
             }
             if(accountBill!=0){
                 System.out.printf("Account Name: "+account.getName()+"\t\t Total paid over period: $%.2f\n", accountBill);
+
+                if(account.getBalance() == 0){
+                    System.out.println("You have paid off "+account.getName()+" balance!!!");
+                }
+                else if((account.getType().equalsIgnoreCase("Balance and Bill")) && account.getBalance()==-1){
+                    System.out.println("You have paid off balance from "+account.getName()+" bill!!");
+                }
+
+                if(account.getBalance()>0){
+                    System.out.printf("You have $%.2f left to pay of balance for "+account.getName()+"!!\n", account.getBalance());
+                }
             }
+
         }
 
 
@@ -119,20 +173,68 @@ public class Main {
     public static List<Account> readFile(Scanner sc, List<Account> accounts){
 
         Account account;
+        String billType = sc.next();
+        String billName = "";
+        int billDate = 0;
+        double billAmount =0;
+        double balanceAmount=0;
+        double newBillAmount=0;
 
-        String billName = sc.next();
 
-        while(!billName.equals("0"))
+        while(!billType.equals("0"))
         {
-            // nameBill/dateBill/priceBill
+//            BalanceAndBill Cellphone 15 69.01 Balance 733.59 PartialBill 46.78
+//            Bill AmazonBundle 16 11.28 NA
+//            Balance BestBuy 12 100.00 Balance 500.00
+//            Bill CarInsurance 1/26 816.00 NA
+//            Bill CarInsurance2 7/26 816.00 NA
+//            Bill CrunchyRoll 30 9.02 NA
+//            0
+
+
             try {
-                account = new Account(billName, sc.nextInt(), sc.nextDouble());
+                billName = sc.next();
+                billDate = sc.nextInt();
+                billAmount = sc.nextDouble();
+
+
+                if(billType.equalsIgnoreCase("BalanceAndBill")){
+                    balanceAmount = sc.nextDouble();
+                    newBillAmount = sc.nextDouble();
+                    account = new Account("Balance and Bill", billName, billDate, billAmount, balanceAmount, newBillAmount);
+                }
+                // Bill
+                else if(billType.equalsIgnoreCase("Balance")){
+                    balanceAmount = sc.nextDouble();
+                    account = new Account(billType, billName, billDate, billAmount, balanceAmount);
+                }
+                else{
+                    account = new Account(billType, billName, billDate, billAmount);
+                }
             }catch(Exception e) {
                 // if "special" date (when payment only occurs on a specific day of the year)
                 String[] stringDate = sc.next().split("/");
                 int month = Integer.parseInt(stringDate[0]);
                 int date = Integer.parseInt(stringDate[1]);
-                account = new Account(billName, date, sc.nextDouble(), month);
+
+                billAmount = sc.nextDouble();
+
+
+                // at this point there is only one scan to be done .... sc.nextDouble for bill..depends on case
+
+                if(billType.equalsIgnoreCase("BalanceAndBill")){
+                    balanceAmount = sc.nextDouble();
+                    newBillAmount = sc.nextDouble();
+                    account = new Account("Balance and Bill", billName, date, billAmount, balanceAmount, newBillAmount, month);
+                }
+                // Bill
+                else if(billType.equalsIgnoreCase("Balance")){
+                    balanceAmount = sc.nextDouble();
+                    account = new Account(billType, billName, date, billAmount, balanceAmount, month);
+                }
+                else{
+                    account = new Account(billType, billName, billDate, billAmount ,month);
+                }
             }
             if(account.getDate() > 28){
                 System.out.println("You're bill date will be changed to the 28th for "+account.getName()+"!");
@@ -140,17 +242,39 @@ public class Main {
             }
             accounts.add(account);
             // keep reading first string of line until "0"
-            billName = sc.next();
+            billType = sc.next();
 
         }
 
+
+       // MAKE METHOD FOR ONLY PRINTING ACCOUNT
         System.out.println("\nYou have the following accounts: ");
         for(Account x : accounts)
         {
             if(x.getMonth()!=0){
-                System.out.printf("Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: "+ x.getMonth()+"/"+ x.getDate() + "\n", x.getCost());
+                if(x.getType().equalsIgnoreCase("BalanceAndBill")){
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: "+ x.getMonth()+"/"+ x.getDate() +
+                            " Remaining Balance: "+x.getBalance() +"\n", x.getCost());
+                }
+                // Bill
+                else if(x.getType().equalsIgnoreCase("Balance")){
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: "+ x.getMonth()+"/"+ x.getDate() + "\n", x.getCost());
+                }
+                else{
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: "+ x.getMonth()+"/"+ x.getDate() + "\n", x.getCost());
+                }
             }else {
-                System.out.printf("Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: " + x.getDate() + "\n", x.getCost());
+                if(x.getType().equalsIgnoreCase("BalanceAndBill")){
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: " + x.getDate() + "\n", x.getCost());
+                }
+                // Bill
+                else if(x.getType().equalsIgnoreCase("Balance")){
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: " + x.getDate() + "\n", x.getCost());
+                }
+                else{
+                    System.out.printf("Bill Type: " + x.getType() + " Name: " + x.getName() + "\t\t Cost: $%.2f\t\t Date: " + x.getDate() + "\n", x.getCost());
+                }
+
             }
         }
         System.out.println("=======================================");
